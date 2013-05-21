9451d736cc90b213b0ea72eef45eaeb5eea1f04e
SimonWu <wuhanyu@gmail.com>
2013-05-21 20:49:48 +0800

    new

---
+++ vpkserver/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vpkserver::Application.config.secret_token = 'b2921a14eae628c23256aa134569d792967e2d569c9e24fd63f00985feaed7bac2a6f9744cfce7ac1e074a12ae0bd7dc217ef0160d2b62a0cb3a2d7f3e16b613'
