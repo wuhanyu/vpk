719f86755d5b2cbf65bef2c966027478d4ff4475
SimonWu <wuhanyu@gmail.com>
2013-05-21 20:52:45 +0800

    mongodb

---
+++ vpkserver/config/initializers/mongo.rb
@@ -0,0 +1,8 @@
+MongoMapper.connection = Mongo::Connection.new('192.168.12.254', 27017)
+MongoMapper.database = "vpkserver-#{Rails.env}"
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    MongoMapper.connection.connect if forked
+  end
+end
